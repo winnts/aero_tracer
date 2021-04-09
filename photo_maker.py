9651449b606c99d6e8fc190abf31b5a05f0603c1
adyachenko <adyachenko@modeln.com>
2021-04-09 12:56:34 +0300

    small fix

--- photo_maker.py
+++ photo_maker.py
@@ -4,7 +4,8 @@ from picamera import PiCamera
 class Camera:
     def __init__(self):
         self.camera = PiCamera()
-        self.camera.rotation = 18
+        self.camera.rotation = 180
+        self.camera.resolution(3280, 2464)
 
     def take_picture(self):
         self.camera.capture("image.jpg")
