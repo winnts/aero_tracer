4a405044ba68a7ba5b879f3f34813bb3d3abfbb5
adyachenko <adyachenko@modeln.com>
2021-04-09 13:18:10 +0300

    small fix

--- web_app.py
+++ web_app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask import send_file
+from aero_tracer import AeroTracer
 
 
 app = Flask(__name__)
+trace = AeroTracer()
+trace.camera_rotating_start()
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/picture')
+def send_picture():
+    return send_file("image.jpg", mimetype='image/jpg')
